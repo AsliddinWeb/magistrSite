f05e43478a122b5a1543b38b321d5ca9e14a4bf0
AsliddinWeb <asliddinsinger@gmail.com>
2024-11-07 11:35:57 +0500

    Initial settings are set

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Global APPS
+
+    # Local APPS
+    'home_app',
 ]
 
 MIDDLEWARE = [
